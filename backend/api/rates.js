6ddd1f97e9ef3e74f9059e250aa4c93c1e1c1470
ayberkenis <ayberkenis@gmail.com>
2024-03-28 03:10:18 +0300

    added rates routes

---
+++ backend/api/rates.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const router = express.Router();
+const connection = require('../db.js');
+
+router.get('/', (req, res) => {
+    connection.query(
+        'SELECT * FROM currency_data ORDER BY Last DESC',
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                res.status(500).send('An error occurred');
+            } else {
+                res.json(results);
+            }
+        }
+    );
+    
+})
+
+router.get('/:code', (req, res) => {
+    connection.query(
+        'SELECT * FROM currency_data WHERE Code = ?',
+        [(req.params.code).toUpperCase()],
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                res.status(500).send('An error occurred');
+            } else {
+                res.json(results);
+            }
+        }
+    );
+})
+
+
+
+
+module.exports = router;
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,21 @@ const express = require('express');
 const app = express();
 const port = 8000;
 app.use(express.json());
-
+const connection = require('./db.js');
+const fs = require('fs');
 /*const sqlFile = require('fs').readFileSync('./_initial.sql').toString();*/
 
 const authRoutes = require('./api/auth.js');
+const ratesRoutes = require('./api/rates.js');
 app.use('/api/auth',  authRoutes)
+app.use('/api/rates', ratesRoutes)
 
+app.get('/', (req, res) => {
+
+    res.json({ message: 'Paratic', version: '0.1', serverTime: new Date(), status: 'OK', uptime: process.uptime()});
+    
+
+});
 app.listen(port, () => {
     console.log(`Paratic Piyasalar Backend listening at http://localhost:${port}`);
     }
