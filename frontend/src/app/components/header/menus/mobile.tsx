4525b86d818bba384fff669314a4b29bc4bd3543
ayberkenis <ayberkenis@gmail.com>
2024-03-29 17:07:39 +0300

    fix mobile navbar

--- frontend/src/app/components/header/menus/mobile.tsx
+++ frontend/src/app/components/header/menus/mobile.tsx
@@ -8,26 +8,29 @@ export default function MobileMenu({ user }: { user: string }) {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
 
     return (
-        <div className="mobile">
+        <>
             <span onClick={() => setShowMobileMenu(true)} className="hamburger-menu-icon">☰</span>
-            <div className={`flex items-center header w-full relative mobile-menu ${showMobileMenu === false ? 'hidden' : ''}`}>
-                <nav className={`nav-mobile ${showMobileMenu === false ? 'hidden' : 'flex'}`}>
-                    <span onClick={() => setShowMobileMenu(false)} className="mt-8 hamburger-menu-icon md:hidden lg:hidden">X</span>
-                    <Image src={logo} alt='Paratic Piyasalar Logo' priority height={32} width={148} className='mt-8 mb-24' />
-                    <span className="navbar-mobile-item">Döviz Kurları</span>
-                    <span className="navbar-mobile-item">Altın Fiyatları</span>
-                    <span className="navbar-mobile-item">Borsa</span>
-                    <span className="navbar-mobile-item">Hisse Senetleri</span>
-                    <span className="navbar-mobile-item">Parite</span>
-                    <span className="navbar-mobile-item">Emtia</span>
-                    <span className="navbar-mobile-item">Kripto</span>
-                    <span className="navbar-mobile-item">Menü</span>
-                    <div className='footer mx-auto w-1/2'>
-                        <Authentication />
-                    </div>
-                </nav>
+            <div className={`mobile ${showMobileMenu === false ? 'hidden' : 'flex'}`}>
 
+                <div className={`flex items-center header w-full relative mobile-menu ${showMobileMenu === false ? 'hidden' : ''}`}>
+                    <nav className={`nav-mobile ${showMobileMenu === false ? 'hidden' : 'flex'}`}>
+                        <span onClick={() => setShowMobileMenu(false)} className="mt-8 hamburger-menu-icon md:hidden lg:hidden">X</span>
+                        <Image src={logo} alt='Paratic Piyasalar Logo' priority height={32} width={148} className='mt-8 mb-24' />
+                        <span className="navbar-mobile-item">Döviz Kurları</span>
+                        <span className="navbar-mobile-item">Altın Fiyatları</span>
+                        <span className="navbar-mobile-item">Borsa</span>
+                        <span className="navbar-mobile-item">Hisse Senetleri</span>
+                        <span className="navbar-mobile-item">Parite</span>
+                        <span className="navbar-mobile-item">Emtia</span>
+                        <span className="navbar-mobile-item">Kripto</span>
+                        <span className="navbar-mobile-item">Menü</span>
+                        <div className='footer mx-auto w-1/2'>
+                            <Authentication />
+                        </div>
+                    </nav>
+
+                </div>
             </div>
-        </div>
+        </>
     )
 }
